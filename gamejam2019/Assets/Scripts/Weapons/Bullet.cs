53e8781ff8b9b954ba2baa3adf563685f6799e1c
Dominic <dominic.mckeith@usask.ca>
2019-01-26 17:51:17 -0600

    Partial implementation of Laser weapon.
    No code has been tested yet.

---
+++ gamejam2019/Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Damage {
+
+    override protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+        Destroy(this.gameObject);
+    }
+}
---
+++ gamejam2019/Assets/Scripts/Weapons/Shooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    [SerializeField] private GameObject laser;
+    [SerializeField] private float fireLength;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    virtual public void Shoot(GameObject origin)
+    {
+        GameObject laser = GameObject.Instantiate(this.laser, this.gameObject.GetComponent<Transform>(), true);
+    }
+}
