d051a3b80889f3955fff6f73575bf6d26c4a145d
Gawesomer <jj.jouband@gmail.com>
2019-01-26 22:14:00 -0600

    Adjusted the follow AI

--- gamejam2019/Assets/Scripts/Movement.cs
+++ gamejam2019/Assets/Scripts/Movement.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour {
     private Rigidbody2D rbody;
     private Transform myTransform;
     private bool goingRight;
-    private Vector2 right, left;
+    private Vector2 right, left, down;
 
     public float velocity;
     public float radius;
@@ -29,6 +29,7 @@ public class Movement : MonoBehaviour {
         goingRight = true;
         right = new Vector2(1, 0);
         left = new Vector2(-1, 0);
+        down = new Vector2(0, -1);
         if (radius == 0)
         {
             radius = 5;
@@ -64,9 +65,13 @@ public class Movement : MonoBehaviour {
     public void Towards(Transform target)
     {
         Vector2 diff = target.position - transform.position;
-        diff.Normalize();
 
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+        if (transform.position.y < target.position.y)
+        {
+            Move(down);
+        }
     }
 }
