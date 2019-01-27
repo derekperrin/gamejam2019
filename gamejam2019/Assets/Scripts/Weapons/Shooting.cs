acd3c00c424a1bb048af3e86362c5e8cc4f9b72d
djmckeith <dominic.mckeith@usask.ca>
2019-01-27 13:23:14 -0600

    Instead of firing a lightsaber, you now shoot bullets every 0.2 seconds. Bullers are no longer extremely thick.

--- gamejam2019/Assets/Scripts/Weapons/Shooting.cs
+++ gamejam2019/Assets/Scripts/Weapons/Shooting.cs
@@ -6,10 +6,11 @@ public class Shooting : MonoBehaviour {
 
     [SerializeField] private GameObject laser;
     [SerializeField] private float fireLength;
+    private bool shootEnabled;
 
 	// Use this for initialization
 	void Start () {
-
+        shootEnabled = true;
     }
 	
 	// Update is called once per frame
@@ -19,8 +20,16 @@ public class Shooting : MonoBehaviour {
 
     virtual public void Shoot()
     {
+        if (!shootEnabled) return;
+        shootEnabled = false;
+        Invoke("enableShoot", fireLength);
         Vector3 startPosition = this.gameObject.GetComponent<Transform>().position;
         GameObject laser = GameObject.Instantiate(this.laser, new Vector3(startPosition.x, startPosition.y, startPosition.z), Quaternion.identity);
         laser.SendMessage("Move", new Vector2(0, 1));
+    }
+
+    private void enableShoot()
+    {
+        shootEnabled = true;
     }
 }
