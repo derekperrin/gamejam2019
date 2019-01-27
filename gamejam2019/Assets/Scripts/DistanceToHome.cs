605e5820cd6f223e34b220728a6fc7b115b99eb3
djmckeith <dominic.mckeith@usask.ca>
2019-01-27 14:14:51 -0600

    Added distance to home countdown.

---
+++ gamejam2019/Assets/Scripts/DistanceToHome.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceToHome : MonoBehaviour {
+
+    [SerializeField] protected Text uiElement;
+    [SerializeField] protected float initialDistanceToHome;
+    [SerializeField] protected float countDownRate;
+    protected float currentDistanceToHome;
+
+	// Use this for initialization
+	void Start () {
+        currentDistanceToHome = initialDistanceToHome;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentDistanceToHome -= countDownRate;
+
+        if (currentDistanceToHome <= 0)
+        {
+            uiElement.text = "YOU ARE HOME!!!!";
+        }
+        else
+        {
+            uiElement.text = "Distance to home: " + currentDistanceToHome.ToString() + " light years.";
+        }
+	}
+}
